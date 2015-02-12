46618a42ef7725563dec1345e180bb63e5751693
polest <a.jaks@gmx.de>
2015-02-12 23:05:12 +0100

    1

---
+++ stats/stats.java
@@ -0,0 +1,15 @@
+public class stats
+{
+    
+    public static void stati(){
+        for(int i = 1; i <= 1000000; i++){
+            IO.println("" + i);
+        }
+    }
+    
+    public static void main(String[] args){
+        IO.println("Das ist Tst");
+        
+        stati();
+    }
+}
